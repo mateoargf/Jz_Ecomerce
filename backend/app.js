ee6fbe9424318c090d65f2f50211488d32d9640d
Mateo Gómez Fleytas <mateogf@hotmail.com>
2024-05-12 03:15:47 -0300

    conexión con la base de datos mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,25 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import connectDB from '../backend/server/database/connectDB.js'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 const port = process.env.PUERTO
+const db = process.env.MONGO_URL
+
+const connect = async () => {
+    try {
+        await connectDB(db)
+        app.listen(port, console.log(`el servidor inició: ${port}`))
+    } catch(error){
+        console.log(`error en el servidror: ${error}`)
+    }
+ }
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo!')
 })
 
-app.listen(port, () => {
-    console.log(`Servidor  Express escuchando el puerto ${port}`)
-})
+connect()
