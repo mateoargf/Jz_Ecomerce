406a8422d9ce6271cdee9239adba40469d11b838
Mateo Gómez Fleytas <mateogf@hotmail.com>
2024-05-15 03:35:17 -0300

    solucion de renderizado con /err/404

--- backend/server/controllers/err.js
+++ backend/server/controllers/err.js
@@ -1,9 +1,3 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 export const get404 = (req,res) => {
-   res.status(404).send('error',res.statusCode)
+   res.status(404).send('error 404')
  } 
