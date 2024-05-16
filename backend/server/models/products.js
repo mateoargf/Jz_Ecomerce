040017aec28ac0a43c7ca81e839fad28524c8e8f
Mateo Gómez Fleytas <mateogf@hotmail.com>
2024-05-16 02:39:45 -0300

    modelado del esquema de productos listo. falta hacer las peticiones post en db

--- backend/server/models/products.js
+++ backend/server/models/products.js
@@ -1,2 +1,47 @@
 import mongoose from 'mongoose'
-const {Schema} = mongoose()
+const { Schema } = mongoose
+
+const products = new Schema({
+     name: {
+          type: String,
+          required: true,
+          validate: {
+               validator: function (value) {
+                    return value.length > 0
+               },
+               message: 'El nombre no puede ir vacío'
+          }
+     },
+     image: {
+          type: String,
+          required: true,
+          validate: {
+               validator: function (value) {
+                    return /\.(jpg|jpeg|png|gif)$/.test(value.toLowerCase())
+               },
+               message: 'La url de la imagen no es válida',
+          }
+     },
+     price: {
+          type: Number,
+          required: true,
+          validate: {
+               validator: function (value) {
+                    return value > 0
+               },
+               message: 'El precio no debe ser menor a 0'
+          }
+     },
+     stock: {
+          type: Number,
+          required: true,
+          validate: {
+               validator: function(value){
+                    return Number.isInteger(value) && value >= 0
+               },
+               message: 'El stock no puede ser menor a 0'
+          }
+     }
+})
+
+export default products
